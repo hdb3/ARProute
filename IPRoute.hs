8dfdf6fb351c080429929213149f152d873b7fe0
Nicholas Hart <nic@hdb3.net>
2018-12-10 16:45:35 +0000

    getPhysical works

--- IPRoute.hs
+++ IPRoute.hs
@@ -1,7 +1,5 @@
 module IPRoute where
 import System.Process
---import Control.Monad(mapM_)
-import Data.List(isPrefixOf)
 
 getNumberedInterfaces :: IO [String]
 getNumberedInterfaces = fmap (map  ( head . words ) . lines ) $ readProcess "ip" ["-4" , "-br" , "addr"] "" 
@@ -11,16 +9,16 @@ getAllInterfaces = fmap (map  ( head . words ) . lines ) $ readProcess "ip" ["-b
 
 getPhysicalInterfaces :: IO [String]
 getPhysicalInterfaces = do
-    let sections = (map words)  . breakOn '\\'
+    let sections = map words . breakOn '\\'
+    -- break up the interface details into word level nested array
     interfaceDetails <- fmap ( map sections . lines) $ readProcess "ip" ["-d" , "-o" , "link"] ""
-    let simpleInterfaces = filter ( (3 > ) . length ) interfaceDetails
-        physicalInterfaces = filter (( isPrefixOf "link/ether" ) . head . ( !! 1 ) ) simpleInterfaces
-    -- print simpleInterfaces
-    -- return $ fmap (( !! 1) . head ) physicalInterfaces
-    return $ fmap (  ( !! 1) . head ) $ filter (( isPrefixOf "link/ether" ) . head . ( !! 1 ) ) $ filter ( (3 > ) . length ) interfaceDetails
-    -- return []
+    -- filter: 1) on number of 'lines' on an interface detail - simple interfaces don't additional lines to specify the sub-type such as bridge / macvtab / tun / etc
+    --         2) to remove the loopback interface and any other odd ones other than real ethernet...
+    -- then finally trim down the output to just field #2 line 1 which is the interface name (lots else available e.g. MAC, ifindex, state, but it is not needed so for simplicity just do this only)
+    return $ fmap (  ( !! 1) . head ) $ filter (( "link/ether" == ) . head . ( !! 1 ) ) $ filter ( (3 > ) . length ) interfaceDetails
 
-breakOn c s = breakOn' (c ==) s where
+breakOn :: Eq a => a -> [a] -> [[a]]
+breakOn c = breakOn' (c ==) where
     breakOn' _ [] = []
     breakOn' p a | p (head a) = breakOn' p (tail a)
-                 | otherwise = (takeWhile (not . p) a) : breakOn' p (dropWhile (not . p) a)
+                 | otherwise = takeWhile (not . p) a : breakOn' p (dropWhile (not . p) a)
