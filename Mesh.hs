1a279b53a1edd0eccb9971fb69ba23740109430b
Nicholas Hart <nic@hdb3.net>
2018-12-12 12:59:23 +0000

    remove active MAC retrieval from active code

--- Mesh.hs
+++ Mesh.hs
@@ -2,14 +2,12 @@ module Main where
 import VirtMesh
 import System.Environment(getArgs)
 import Control.Monad(mapM)
-import Data.List(zip)
+import Data.List(zip,repeat)
 --import Data.Maybe(fromJust)
 
 main = do
     hx <- getArgs
-    interfaces <- mapM getDomainDirectInterfaceData hx
-    let macs = map (\(Just (x,_,_)) -> x) interfaces
-        hostMacs = zip hx macs
+    hostMacs <- gethostMacs' hx 
     print hostMacs
     let makeMesh l = [(i,j) | i<-l, j<-l, i<j]
     let -- mesh = [(i,j) | i<-hx, j<-hx, i<j]
@@ -19,3 +17,11 @@ main = do
         unlinks = map (\(i,j) -> unlink i j) (makeMesh hostMacs)
     putStrLn $ unlines links
     putStrLn $ unlines unlinks
+
+gethostMacs' hx = do
+    return $ zip hx (repeat "")
+
+gethostMacs hx = do
+    interfaces <- mapM getDomainDirectInterfaceData hx
+    let macs = map (\(Just (x,_,_)) -> x) interfaces
+    return $ zip hx macs
