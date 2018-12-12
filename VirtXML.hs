8a8a8ac40ac4e276891ad2bcfb275d0310f6a808
Nicholas Hart <nic@hdb3.net>
2018-12-12 12:13:07 +0000

    add VirtXML-test.hs, make VirtXML.hs a module

--- VirtXML.hs
+++ VirtXML.hs
@@ -1,17 +1,14 @@
-module Main where
---import System.Environment(getArgs)
+module VirtXML where
 import Text.XML.Light
 import Data.Maybe
 import qualified Data.ByteString
 
-
-main = do
-    c <- Data.ByteString.getContents
-    let g = head $ onlyElems $ parseXML c
-        pDirect = ( "direct" == ) . fromJust . findAttr ( QName "type" Nothing Nothing )
-        directInterfaces = filter pDirect $ findElements ( QName "interface" Nothing Nothing ) $ fromJust $ findElement ( QName "devices" Nothing Nothing ) g
-        direct = head directInterfaces
-        mac = fromJust . findAttr ( QName "address" Nothing Nothing ) . fromJust . findElement ( QName "mac" Nothing Nothing )
-        name = fromJust . findAttr ( QName "name" Nothing Nothing ) . fromJust . findElement ( QName "alias" Nothing Nothing )
-        source = fromJust . findAttr ( QName "dev" Nothing Nothing ) . fromJust . findElement ( QName "source" Nothing Nothing )
-    print (mac direct , name direct , source direct)
+getDirectInterfaceData :: Data.ByteString.ByteString -> Maybe (String, String, String)
+getDirectInterfaceData c = if 0 < length directInterfaces then Just (mac direct , name direct , source direct) else Nothing where
+    g = head $ onlyElems $ parseXML c
+    pDirect = ( "direct" == ) . fromJust . findAttr ( QName "type" Nothing Nothing )
+    directInterfaces = filter pDirect $ findElements ( QName "interface" Nothing Nothing ) $ fromJust $ findElement ( QName "devices" Nothing Nothing ) g
+    direct = head directInterfaces
+    mac = fromJust . findAttr ( QName "address" Nothing Nothing ) . fromJust . findElement ( QName "mac" Nothing Nothing )
+    name = fromJust . findAttr ( QName "name" Nothing Nothing ) . fromJust . findElement ( QName "alias" Nothing Nothing )
+    source = fromJust . findAttr ( QName "dev" Nothing Nothing ) . fromJust . findElement ( QName "source" Nothing Nothing )
