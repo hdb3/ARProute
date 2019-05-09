807097463e7b9731508e03abefb7f6d6654fd7b3
nic hart <nic@hdb3.net>
2019-05-09 12:31:42 +0100

    WiP

--- IPRoute.hs
+++ IPRoute.hs
@@ -96,13 +96,13 @@ getNumberedInterfaces :: IO [String]
 getNumberedInterfaces = fmap (map  ( head . words ) . lines ) $ readProcess "ip" ["-4" , "-br" , "addr"] "" 
 
 getAllInterfaces :: IO [String]
-getAllInterfaces = fmap (map  ( head . words ) . lines ) $ readProcess "ip" ["-br" , "link"] "" 
+getAllInterfaces = fmap (map  ( head . words ) . lines ) $ readProcess "ip" ["-br" , "link", "show" , "up"] "" 
 
 getPhysicalInterfaces :: IO [String]
 getPhysicalInterfaces = do
     let sections = map words . breakOn '\\'
     -- break up the interface details into word level nested array
-    interfaceDetails <- fmap ( map sections . lines) $ readProcess "ip" ["-d" , "-o" , "link"] ""
+    interfaceDetails <- fmap ( map sections . lines) $ readProcess "ip" ["-d" , "-o" , "link", "show" , "up"] ""
     -- filter: 1) on number of 'lines' on an interface detail - simple interfaces don't additional lines to specify the sub-type such as bridge / macvtab / tun / etc
     --         2) to remove the loopback interface and any other odd ones other than real ethernet...
     -- then finally trim down the output to just field #2 line 1 which is the interface name (lots else available e.g. MAC, ifindex, state, but it is not needed so for simplicity just do this only)
