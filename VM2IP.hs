125d60834df112752ec842410e394df787371efa
nic hart <nic@hdb3.net>
2019-06-04 13:44:30 +0100

    integrated inotify wait in vm2ip

--- VM2IP.hs
+++ VM2IP.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
+import System.IO(stderr, hPutStrLn)
 import System.Process
 import System.Environment(getArgs)
 import System.Exit
 import VirtXML
 import LibvirtIP
+import Notify
 
 main :: IO()
 main = do
@@ -14,9 +16,16 @@ main = do
     else do
         xml <- readProcess "virsh" ["dumpxml" , head args] ""
         let mac = getNetworkInterfaceMAC xml
-        --putStrLn $ "MAC found: " ++ mac
-        recs <- mac2AddressRecords mac
-        if 1 /= length recs then
-            die "fail"
-        else
+        reportIP mac
+    where
+    reportIP m = do
+        recs <- mac2AddressRecords m
+        if 1 == length recs then
             print (ipAddress $ head recs)
+        else do
+            hPutStrLn stderr "waiting on virbr0.status"
+            watch <- setWatchModify "/var/lib/libvirt/dnsmasq/virbr0.status"
+            flag <- getWatch watch 20
+            if flag then reportIP m
+            else
+                die "timed out waiting for an IP address"
