ef4bdb1cdc3752a6413a0eaa7d0177136f1dccd5
nic hart <nic@hdb3.net>
2019-06-07 16:47:08 +0100

    wip

--- buildvm.sh
+++ buildvm.sh
@@ -1,19 +1,18 @@
-#!/bin/bash
-export vm=$1 memsize=$2 cpucount=$3 sourceimage=CentOS-7-x86_64-GenericCloud.qcow2 pool=libvirt
+#!/bin/bash -xe
+export vm=$1 memsize=$2 cpucount=$3 sourceimage=CentOS-7-x86_64-GenericCloud pool=libvirt
 #export vm=h55 memsize=2048 cpucount=1 sourceimage=CentOS-7-x86_64-GenericCloud.qcow2 pool=libvirt
-echo "virsh vol-clone --pool $pool $sourceimage ${vm}.qcow2"
-virsh vol-clone --pool $pool $sourceimage ${vm}.qcow2
-echo "virt-install --name $vm --memory $memsize --vcpus $cpucount --disk ${vm}.qcow2 --import --os-variant rhel7 --noautoconsole"
-virt-install --name $vm --memory $memsize --vcpus $cpucount --disk vol=$pool/${vm}.qcow2 --import --os-variant rhel7 --noautoconsole
+echo "virsh vol-clone --pool $pool $sourceimage ${vm}"
+virsh vol-clone --pool $pool $sourceimage ${vm}
+echo "virt-install --name $vm --memory $memsize --vcpus $cpucount --disk ${vm} --import --os-variant rhel7 --noautoconsole"
+virt-install --name $vm --memory $memsize --vcpus $cpucount --disk vol=$pool/${vm} --import --os-variant rhel7 --noautoconsole
 export remoteip=`vm2ip $vm`
 echo "IP for $vm is $remoteip"
-#echo "IP for $vm is `vm2ip $vm` "
-ssh centos@${remoteip} sudo yum -y update 
-ssh centos@${remoteip} sudo yum -y install docker
-scp docker-network centos@${remoteip}:
-ssh centos@${remoteip} sudo mv docker-network /etc/sysconfig/docker-network
-ssh centos@${remoteip} sudo systemctl enable --now docker
-ssh centos@${remoteip} sudo docker info
-DOCKER_HOST=${remoteip} docker info
 printf "server big 8053 \n update add ${vm}.virt 86400 A ${remoteip} \n send\n" | nsupdate
-ping ${vm}.virt
+fping ${vm}.virt
+ssh centos@${vm} sudo yum -y update 
+ssh centos@${vm} sudo yum -y install docker
+scp docker-network centos@${vm}:
+ssh centos@${vm} sudo mv docker-network /etc/sysconfig/docker-network
+ssh centos@${vm} sudo systemctl enable --now docker
+ssh centos@${vm} sudo docker info
+DOCKER_HOST=${vm} docker info
--- make.sh
+++ make.sh
@@ -1,6 +1,7 @@
 cabal install --only-dependencies
 cabal configure
 cabal build
+cabal install
 mkdir -p arproute
 ln  arproute.service install.sh dist/build/arprouted/arprouted arproute
 tar czf arproute.tgz arproute
