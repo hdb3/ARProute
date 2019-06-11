59068aac46375cddc1aca3d534f9cf57bc90bdf1
nic hart <nic@hdb3.net>
2019-06-11 11:00:57 +0100

    version of buildvm now in use for real work

--- buildvm.sh
+++ buildvm.sh
@@ -9,6 +9,12 @@ fping ${vm}.virt
 ssh centos@${vm} sudo yum -y check-update || echo "non-zero return ignored"
 ssh centos@${vm} sudo "curl -fsSL https://get.docker.com/ | sh"
 ssh centos@${vm} sudo "sed -i.original -e'/^ExecStart=\/usr\/bin\/dockerd/s/$/ -H tcp:\/\/0.0.0.0:2375/' /usr/lib/systemd/system/docker.service"
+scp daemon.json centos@${vm}:
+ssh centos@${vm} sudo install -D daemon.json /etc/docker/daemon.json
 ssh centos@${vm} sudo systemctl enable --now docker
 ssh centos@${vm} sudo docker info
 DOCKER_HOST=${vm} docker info
+scp arproute/arprouted arproute/arproute.service centos@${vm}:
+ssh centos@${vm} sudo mv arprouted /usr/sbin
+ssh centos@${vm} sudo mv arproute.service /usr/lib/systemd/system
+ssh centos@${vm} sudo systemctl enable --now arproute
